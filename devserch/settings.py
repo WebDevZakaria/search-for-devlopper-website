748d237a0aa6ef59c21d09d2c60f40abf5257632
zaki <zikoubouregbi@gmail.com>
2023-03-10 21:59:01 +0100

    deployy

--- devserch/settings.py
+++ devserch/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-46c-=wt03bhj13mzij5ea)o_4^#7bls9@#us6$*xaznsiv8(uz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
